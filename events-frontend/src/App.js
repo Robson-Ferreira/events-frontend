431be496db026e3bca2d4d6946640e79487b0e02
matheushenriquecsb <matheus.mhg2@gmail.com>
2023-06-03 08:46:34 -0300

    feat: create setup Auth0 and components login, logout and user

---
+++ events-frontend/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import User from "./components/User";
+
+function App() {
+  const { isAuthenticated } = useAuth0();
+
+  return (
+    <div className="App">
+      {!isAuthenticated ? (
+        <div>
+          <LoginButton />
+        </div>
+      ) : (
+        <div>
+          <LogoutButton />
+          <User />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ events-frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>
+);
